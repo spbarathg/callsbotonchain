0005693bdf5b24ebea00c3f9eaea86bdc4233ea0
Barath Ganesh <barathganesh64@gmail.com>
2025-10-22 03:13:03 +0530

    chore: cleaning up unnecessary files

--- get_wallet.py
+++
@@ -1,8 +0,0 @@
-from tradingSystem.config_optimized import WALLET_SECRET
-from solders.keypair import Keypair
-import base58 as b58
-
-kp = Keypair.from_bytes(b58.b58decode(WALLET_SECRET.strip()))
-print(f"Wallet Address: {kp.pubkey()}")
-
-
